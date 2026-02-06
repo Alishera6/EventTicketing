023035b9d8c81c7df3d03395e9724d9552c795a1
liamood233 <liamood234@gmail.com>
2026-02-06 19:45:09 +0600

    Add Builder class to Event entity

--- src/entities/Event.java
+++ src/entities/Event.java
@@ -1,6 +1,7 @@
 package entities;
 
 public class Event {
+
     private int id;
     private String name;
     private String date;
@@ -33,5 +34,37 @@ public class Event {
 
     public int getVenueId() {
         return venueId;
+    }
+
+    // ===== BUILDER STARTS HERE =====
+    public static class Builder {
+        private int id;
+        private String name;
+        private String date;
+        private int venueId;
+
+        public Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder date(String date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder venueId(int venueId) {
+            this.venueId = venueId;
+            return this;
+        }
+
+        public Event build() {
+            return new Event(id, name, date, venueId);
+        }
     }
 }
